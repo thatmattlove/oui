2a4fe4f47189dd1621660772c36183d7960309d9
thatmattlove <mtt.lov@gmail.com>
2022-01-19 13:13:56 -0700

    add and refactor tests

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 require (
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/errors v0.20.1 // indirect
 	github.com/go-openapi/strfmt v0.21.1 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
@@ -24,10 +25,14 @@ require (
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	go.mongodb.org/mongo-driver v1.8.2 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- util_test.go
+++ util_test.go
@@ -1,58 +1,68 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_removeComments(t *testing.T) {
-
-	tests := [][]string{
-		{"stuff # with comment", "stuff"},
-		{"# line with comment", ""},
-		{`escaped \# comment`, `escaped \# comment`},
-		{"#", ""},
-		{`00:00:D1	Adaptec		Adaptec, Inc.	# "Nodem" product`, `00:00:D1	Adaptec		Adaptec, Inc.`},
-	}
-	for i, p := range tests {
-		t.Run(fmt.Sprintf("removeComments %d", i+1), func(t *testing.T) {
-			er := p[1]
-			r := removeComments(p[0])
-			if r != er {
-				t.Errorf("'%v' != '%v'", r, er)
-			}
-		})
-	}
+	t.Run("removeComments - inline", func(t *testing.T) {
+		assert.Equal(t, "stuff", removeComments("stuff # with comment"))
+	})
+	t.Run("removeComments - full line", func(t *testing.T) {
+		assert.Equal(t, "", removeComments("# line with comment"))
+	})
+	t.Run("removeComments - escaped", func(t *testing.T) {
+		assert.Equal(t, `escaped \# comment`, removeComments(`escaped \# comment`))
+	})
+	t.Run("removeComments - empty line", func(t *testing.T) {
+		assert.Equal(t, "", removeComments("#"))
+	})
+	t.Run("removeComments - end comment", func(t *testing.T) {
+		r := removeComments(`00:00:D1	Adaptec		Adaptec, Inc.	# "Nodem" product`)
+		e := `00:00:D1	Adaptec		Adaptec, Inc.`
+		assert.Equal(t, e, r)
+	})
 }
 
 func Test_splitTabs(t *testing.T) {
 	t.Run("splitTabs 1", func(t *testing.T) {
 		r := splitTabs(`one	two	three`)
 		e := []string{"one", "two", "three"}
-		if !reflect.DeepEqual(r, e) {
-			t.Errorf("'%v' != '%v'", r, e)
-		}
+		assert.Equal(t, e, r)
 	})
 	t.Run("splitTabs 2", func(t *testing.T) {
 		r := splitTabs(`one	two		three`)
 		e := []string{"one", "two", "three"}
-		if !reflect.DeepEqual(r, e) {
-			t.Errorf("'%v' != '%v'", r, e)
-		}
+		assert.Equal(t, e, r)
 	})
 	t.Run("splitTabs 3", func(t *testing.T) {
 		r := splitTabs(`one		two		three`)
 		e := []string{"one", "two", "three"}
-		if !reflect.DeepEqual(r, e) {
-			t.Errorf("'%v' != '%v'", r, e)
-		}
+		assert.Equal(t, e, r)
 	})
 	t.Run("splitTabs 4", func(t *testing.T) {
 		r := splitTabs(`one		two		three	`)
 		e := []string{"one", "two", "three"}
-		if !reflect.DeepEqual(r, e) {
-			t.Errorf("'%v' != '%v'", r, e)
+		assert.Equal(t, e, r)
+	})
+}
+
+func Test_pathExists(t *testing.T) {
+	t.Run("pathExists - not exists", func(t *testing.T) {
+		r := pathExists("/this/path/does/not/exist")
+		assert.False(t, r)
+	})
+	t.Run("pathExists - exists", func(t *testing.T) {
+		tf, err := os.CreateTemp(os.TempDir(), "oui-test-*")
+		if err != nil {
+			panic(err)
 		}
+		defer os.Remove(tf.Name())
+
+		r := pathExists(tf.Name())
+		assert.True(t, r)
 	})
 }
